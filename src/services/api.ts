36250fe0331ad58a3e3f0f9d700b8e64f5422001
CrystalNyein <nyeinchanlay99@gmail.com>
2025-01-22 09:53:22 +0630

    Server setup to allow https

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,10 @@
-import axios from 'axios';
-import { storageKey } from '../constants/storage';
+import axios from "axios";
+import { storageKey } from "../constants/storage";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_ENDPOINT,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
@@ -13,17 +13,17 @@ api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem(storageKey.TOKEN);
     if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
+      config.headers["Authorization"] = `Bearer ${token}`;
     }
     return config;
   },
-  (error) => Promise.reject(error),
+  (error) => Promise.reject(error)
 );
 api.interceptors.response.use(
   (response) => response,
   (error) => {
     return Promise.reject(error.response);
-  },
+  }
 );
 
 export default api;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, "key.pem")),
+      cert: fs.readFileSync(path.resolve(__dirname, "cert.pem")),
+    },
+    proxy: {
+      "/api": {
+        target: "https://DESKTOP-CAQ0N38.local:8080",
+        changeOrigin: true,
+        secure: false, // Accept self-signed certificates
+      },
+    },
+  },
   plugins: [react()],
-})
+});
